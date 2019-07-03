72ae81c74a5c7d2bb7a92929d5e7c49545939d3d
sadhudgp91 <st158332@stud.uni-stuttgart.de>
2019-07-03 16:57:51 +0200

    connect to postgres database and test data

--- Anträge/settings.py
+++ Anträge/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'Anträge.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',                      
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- app/views.py
+++ app/views.py
@@ -4,7 +4,22 @@ Definition of views.
 
 from datetime import datetime
 from django.shortcuts import render
+from .models import Post
 from django.http import HttpRequest
+
+
+def createpost(request):
+        if request.method == 'POST':
+            if request.POST.get('sapname') and request.POST.get('vorname'):
+                post=Post()
+                post.title= request.POST.get('sapname')
+                post.content= request.POST.get('vorname')
+                post.save()
+                
+                return render(request, 'app/index.html')  
+
+        else:
+                return render(request,'app/index.html')
 
 def home(request):
     """Renders the home page."""
